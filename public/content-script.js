cdb355d990a04de7658e0534dd59fa1cfe893923
François Hersent <hersentino@gmail.com>
2021-09-15 11:21:10 +0200

    stream working with observable

--- public/content-script.js
+++ public/content-script.js
@@ -12,11 +12,9 @@ function injectors(msgSource) {
     SERVER_STREAMING: 'server_streaming'
   };
 
-  const requestMethodName = (req) => req?.getMethodDescriptor().name ?? 'Unknown'
-
   const postMessage = (type, name, reqMsg, resMsg, error) => {
-    const request = reqMsg?.toObject();
-    const response = error ? undefined : resMsg?.toObject();
+    const request = reqMsg;
+    const response = error ? undefined : resMsg;
     error = error ? (({code, message}) => ({ code, message }))(error) : error;
     const msg = {
       source: msgSource,
@@ -24,111 +22,83 @@ function injectors(msgSource) {
       method: name,
       request,
       response,
-      error
+      error,
+      
     };
-    window.postMessage(msg, '*');
+    window.postMessage(JSON.parse(JSON.stringify(msg)), '*');
   }
 
   class DevToolsUnaryInterceptor {
     type = MethodType.UNARY;
 
     postResponse = (name, req, res) => {
-      postMessage(this.type, name, req?.getRequestMessage(), res?.getResponseMessage());
+      postMessage(this.type, name, req, res);
       return res;
     }
 
     postError = (name, req, error) => {
       if (error.code === 0) return error;
-      postMessage(this.type, name, req?.getRequestMessage(), undefined, error);
+      postMessage(this.type, name, req, undefined, error);
       return error;
     }
 
-    intercept = (request, invoker) => {
-      const name = requestMethodName(request);
-      return invoker(request)
-        .then((response) => this.postResponse(name, request, response))
-        .catch((error) => { throw this.postError(name, request, error) });
+    intercept = async (request, invoker, grpcGatewayUrl, serviceName, functionName) => {
+      const name = `${grpcGatewayUrl}/${serviceName}/${functionName}`; 
+
+      try {
+        const response = await invoker(request);
+        this.postResponse(name, request, response)
+        return response
+      } catch (error){
+        throw this.postError(name, request, error);
+      }
     }
   }
 
   class DevToolsStreamInterceptor {
-
-    intercept(request, invoker) {
-
-      class InterceptedStream {
-        type = MethodType.SERVER_STREAMING;
-        name;
-        stream;
-
-        constructor(request, invoker) {
-          this.name = requestMethodName(request);
-          this.stream = invoker(this.postStreamRequest(request));
-        };
-
-        postStreamRequest = (req) => {
-          postMessage(this.type, this.name, req?.getRequestMessage());
-          return req;
-        }
-
-        postStreamData = (data) => {
-          postMessage(this.type, this.name, undefined, data);
-          return data;
-        }
-
-        postStreamError = (error) => {
-          if (error.code === 0) return error;
-          postMessage(this.type, this.name, undefined, undefined, error);
-          return error;
-        }
-
-        postStreamStatus = (status) => {
-          if (status.code !== 0) return status;
-          // FIXME: this is a semi-ugly hack. We add
-          //  toObject() => 'EOF' to the status object
-          //  to mimic an reqMes and make postMessage
-          //  happy. In the future, maybe status could
-          //  be handled properly.
-          status.toObject = () => 'EOF';
-          return this.postStreamData(status);
-        }
-
-        on = (eventType, callback) => {
-          if (eventType === 'data') {
-            const dataCallback = (data) => {
-              callback(this.postStreamData(data));
-            }
-            this.stream.on(eventType, dataCallback);
-          } else if (eventType === 'error') {
-            const errorCallback = (error) => {
-              callback(this.postStreamError(error));
-            }
-            this.stream.on('error', errorCallback);
-          } else if (eventType === 'metadata') {
-            this.stream.on('metadata', callback);
-          } else if (eventType === 'status') {
-            const statusCallback = (status) => {
-              callback(this.postStreamStatus(status));
-            }
-            this.stream.on('status', statusCallback);
-          } else if (eventType === 'end') {
-            this.stream.on('end', callback);
-          }
-          return this;
-        };
-
-        removeListener(eventType, callback) {
-        }
-
-        cancel() {
-        }
+ 
+    intercept(request, invoker, grpcGatewayUrl, serviceName, functionName) {
+      function postStreamRequest(req, name, type) {
+        postMessage(type, name, req);
+        return req;
+      }
+  
+      function postStreamData(data, name, type){
+        postMessage(type, name, undefined, data);
+        return data;
+      }
+  
+      function postStreamError (error, name, type) {
+        if (error.code === 0) return error;
+        postMessage(type, name, undefined, undefined, error);
+        return error;
       }
 
-      return new InterceptedStream(request, invoker);
-    };
+      function  postStreamStatus(name, type) {
+        const status  = "EOF"
+        return postStreamData(status, name, type);
+      } 
+
+      const arr = [];
+      const type = MethodType.SERVER_STREAMING;
+      const name = `${grpcGatewayUrl}/${serviceName}/${functionName}`;  
+      
+      postStreamRequest(request, name, type)
+      const stream = invoker(request);
+        
+      stream.subscribe({
+        next(x) { 
+          postStreamData(x, name, type);
+          arr.push(arr)
+        },
+        error(err) { postStreamError(err, name, type) },
+        complete() { postStreamStatus(name, type) }     
+      }); 
+      return stream
+    }
   }
 
   return {
-    // this is how it should work
     devToolsUnaryInterceptor: new DevToolsUnaryInterceptor(),
     devToolsStreamInterceptor: new DevToolsStreamInterceptor(),
   };
